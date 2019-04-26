35ccae6c6d72bc40401b433ddbabdda51b242da0
wac <wuanch@gmail.com>
2019-04-26 14:43:47 +0800

    fix

--- text_process.py
+++ text_process.py
@@ -49,7 +49,7 @@ class GroceryTextPreProcessor(object):
     def __init__(self, stopwords_mode=False,
                  keywords_mode=True,#keywords default True
                  POS_mode=True,
-                 bert_mode=True):
+                 bert_mode=False):
 
         # index must start from 1
         self.tok2idx = {'>>dummy<<': 0}
