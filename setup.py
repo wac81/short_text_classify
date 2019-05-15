90c83dd7eeda96121c78b2a9f3e4cff0705ec262
wac <wuanch@gmail.com>
2019-05-15 16:03:43 +0800

    fix

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ with open('README.md') as f:
 
 class InstallCommand(install):
     def run(self):
+        os.system('make')
         common_dir = 'stclassify/svc_impl'
         libpostfix = '.dll' if os.name == 'nt' else '.so.1'
         #cp = 'copy' if os.name == 'nt' else 'cp'
@@ -43,7 +44,7 @@ class MakeAndInstallCommand(InstallCommand):
 
 setuptools.setup(
     name="stclassify",
-    version="0.1.0.13",
+    version="0.1.0.17",
     author="wac",
     author_email="wuanch@gmail.com",
     description="short_text_classify",
@@ -60,6 +61,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    # cmdclass={'install': MakeCommand}
+    # cmdclass={'install': InstallCommand}
+    # cmdclass={'make_and_install': MakeAndInstallCommand}
+
     cmdclass={'install': InstallCommand, 'make_and_install': MakeAndInstallCommand}
 )
