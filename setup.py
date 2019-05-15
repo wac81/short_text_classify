1761377c8d3f6697700ae6fed67e7a2fcac65436
wac81 <wuanch@gmail.com>
2019-05-15 15:14:58 +0800

    add build support win

--- setup.py
+++ setup.py
@@ -1,30 +1,45 @@
 import setuptools
-
 import os
-
 from setuptools.command.install import install
-
-from setuptools import setup
-
 
 with open('README.md') as f:
     LONG_DESCRIPTION = f.read()
 
+# class MakeCommand(install):
+#     def run(self):
+#         os.system('make')
+#         common_dir = 'stclassify/svc_impl'
+#         target_dir = '%s/%s' % (self.build_lib, common_dir)
+#         print(self.build_lib, target_dir)
+#         self.mkpath(target_dir)
+#         os.system('cp %s/util.so.1 %s' % (common_dir, target_dir))
+#         common_dir = common_dir + '/liblinear'
+#         target_dir = '%s/%s' % (self.build_lib, common_dir)
+#         self.mkpath(target_dir)
+#         os.system('cp %s/liblinear.so.1 %s' % (common_dir, target_dir))
+#         install.run(self)
 
-class MakeCommand(install):
+class InstallCommand(install):
     def run(self):
-        os.system('make')
         common_dir = 'stclassify/svc_impl'
-        target_dir = '%s/%s' % (self.build_lib, common_dir)
-        print(self.build_lib, target_dir)
-        self.mkpath(target_dir)
-        os.system('cp %s/util.so.1 %s' % (common_dir, target_dir))
-        common_dir = common_dir + '/liblinear'
+        libpostfix = '.dll' if os.name == 'nt' else '.so.1'
+        #cp = 'copy' if os.name == 'nt' else 'cp'
         target_dir = '%s/%s' % (self.build_lib, common_dir)
         self.mkpath(target_dir)
-        os.system('cp %s/liblinear.so.1 %s' % (common_dir, target_dir))
+        self.copy_file('%s/util%s' % (common_dir, libpostfix), target_dir)
+        #os.system('cp %s/util.%s %s' % (common_dir, libpostfix, target_dir))
+        common_dir = 'stclassify/svc_impl/liblinear'
+        target_dir = '%s/%s' % (self.build_lib, common_dir)
+        self.mkpath(target_dir)
+        self.copy_file('%s/liblinear%s' % (common_dir, libpostfix), target_dir)
+        #os.system('cp %s/liblinear.%s %s' % (common_dir, libpostfix, target_dir))
         install.run(self)
 
+class MakeAndInstallCommand(InstallCommand):
+    def run(self):
+        makecmd = 'nmake -f Makefile.win clean liball' if os.name == 'nt' else 'make'
+        os.system(makecmd)
+        InstallCommand.run(self)
 
 setuptools.setup(
     name="stclassify",
@@ -45,6 +60,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    cmdclass={'install': MakeCommand}
-
+    # cmdclass={'install': MakeCommand}
+    cmdclass={'install': InstallCommand, 'make_and_install': MakeAndInstallCommand}
 )
